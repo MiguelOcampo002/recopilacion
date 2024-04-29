4077562bf41e93c1321ab59ca7ba99cbd0dba717
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-04-29 16:25:48 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("Recopilación de trabajos en clase, Miguel Ocampo")
 st.subheader("Esta página te permitirá ver el progreso del trabajo en clase de inteligencia computacional")
-st.image("DSC09768.jpg")
+st.image("DSC09768.JPG")
