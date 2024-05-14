2ad150b850387f63d8616d04b37a27f897f9b33d
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-05-14 13:37:01 -0500

    Create 4-realidadextendida.py

---
+++ pages/4-realidadextendida.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title ("Las realidades extendidas")
+st.image
+st.subheader ("Las realidades extendidas permiten un comprendimiento físico espacial de lo que deseemos mostrar desde cualquier parte del mundo gracias a los dispositivos móviles y de realidad aumentada que cada día son mas acsesibles en el mercado, lo que potencia la capacidad de información que transmitimos en nuestro día a día a través de métodos tridimencionales")
+
+st.write ("link de la página de ejercicio en clase: 
