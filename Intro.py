c38b1da64666ce680bb7e74d8d3fc6959245bf74
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-05-14 09:15:00 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title("Recopilación de trabajos en clase, Miguel Ocampo")
 st.subheader("Esta página te permitirá ver el progreso del trabajo en clase de inteligencia computacional")
 st.image("DSC09768.JPG")
+st.write("El curso de inteligencia urbana en su aspecto computacional nos ha permitido ver la ciudad desde la toma y análisis de datos por medio de distintos métoddos. El curso no sha guíado a través de las herramientas de inteligencia artificial y analítica de datos que la computación permite, donde estos ejercicios desarrollados en clase se encuentran en esta página web recopilatoria")
