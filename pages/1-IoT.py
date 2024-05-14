21d7b6e872b8d7088613fb14e3d3e5b77d2c4fb1
MiguelOcampo002 <163178570+MiguelOcampo002@users.noreply.github.com>
2024-05-14 09:17:45 -0500

    Update 1-IoT.py

--- pages/1-IoT.py
+++ pages/1-IoT.py
@@ -6,7 +6,8 @@ from PIL import Image
 st.title('Temperatura y humedad, simulación irreal')
 image = Image.open('OIP.jpeg')
 st.image(image)
-st.subheader ("Esta primera página te mostrará los calculos básicos de los datos ingresados por csv sacados de la medición de los sensores de humedad y temperatura simulados")
+st.subheader("la comprensión de los datos pueden potenciar la toma de desiciones de una ciudad")
+st.write ("Esta primera página te mostrará los calculos básicos de los datos ingresados por csv sacados a través de la medición de sensores de humedad y temperatura simulados como todo un proyecto de IoT, esto tiene la potencialidad de transformarse en un modelo de análisis automático para tomas de desiciones reactivas y proactivas a según el interés del desarrollador")
 
 uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
